97ca95e43998f0d912a1303602b03528af9be3fc
Miguel Fuertes <hkfuertes@gmail.com>
2020-08-29 22:58:22 +0100

    Changes mvp 1.0

--- daemon/Helpers/MappingHelper.py
+++ daemon/Helpers/MappingHelper.py
@@ -2,14 +2,14 @@ import sqlite3, json
 
 TABLE_NAME = "remote_mapping"
 
-def read_from_db(connection_cursor, ir):
-    connection_cursor.execute('SELECT config FROM '+TABLE_NAME + " WHERE active = 1 and ir = " + str(int(ir==True)))
+def read_from_db(connection_cursor, platform = 'a'):
+    connection_cursor.execute('SELECT config FROM '+TABLE_NAME + " WHERE active = 1 and platform = '" + str(platform) + "'")
     return connection_cursor.fetchone()[0]
 
-def getMappingFromDB(database_path, ir = True):
+def getMappingFromDB(database_path, platform='a'):
     conn = sqlite3.connect(database_path)
     c = conn.cursor()
-    data = json.loads(read_from_db(c, ir))
+    data = json.loads(read_from_db(c, platform))
     c.close()
     return data
 
